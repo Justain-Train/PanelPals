54eca8a0c9a7b49cc08900ba14ec2c2ab6b24637
Justin Tran <justintran952@gmail.com>
2024-06-06 21:01:54 -0400

    Pulled backend files back to pure Javascript

---
+++ src/backend/controller/text_to_speech_file.js
@@ -0,0 +1,34 @@
+const { ElevenLabsClient } = require('elevenlabs');
+const { createWriteStream } = require('fs');
+require('dotenv').config();
+
+const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
+
+const client = new ElevenLabsClient({
+  apiKey: ELEVENLABS_API_KEY,
+});
+
+export const createAudioFileFromText = async (text) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const audio = await client.generate({
+        voice: "Callum",
+        model_id: "eleven_turbo_v2",
+        text,
+      });
+      const fileName = `output.wav`;
+      const fileStream = createWriteStream(fileName);
+
+      audio.pipe(fileStream);
+      fileStream.on("finish", () => resolve(fileName)); // Resolve with the fileName
+      fileStream.on("error", reject);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+
+
+
+
