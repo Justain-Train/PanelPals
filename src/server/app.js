52ef701acb4e3d503bb2dce1d9418bec48a2a772
Knightn4 <Justintran952@gmail.com>
2024-06-09 17:59:13 -0500

    Added a bodyparser

--- src/server/app.js
+++ src/server/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 const audioRoutes = require('./routes/audioRoutes');
 const textRoutes = require('./routes/textRoutes');
 
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json({ limit: '10mb' })); // Increase the limit as per your requirement
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true })); // Increase the limit as per your requirement
 
 
 app.use('/api', audioRoutes); 
