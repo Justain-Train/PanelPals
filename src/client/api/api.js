4950f50e2b31627957cfa112720d22596b4a5dbf
Justin Tran <justintran952@gmail.com>
2024-06-12 04:05:02 -0400

    chore: Refactor API code for file uploading and text detection

--- src/client/api/api.js
+++ src/client/api/api.js
@@ -15,23 +15,18 @@ export const createAudioFileFromText = async (text) => {
     }
 
     const blob = await response.blob();
-    const audioUrl = URL.createObjectURL(blob);
-  
-    return audioUrl; // Return the URL for the audio file
+    return blob; // Return the URL for the audio file
   } catch (error) {
     console.error(error);
     throw error; // Re-throw the error for further handling
   }
 };
 
-export const detectText = async (image) => {
+export const detectText = async (formData) => {
   try {
     const response = await fetch('http://localhost:3000/api/detect-text', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ image }),
+      body: formData,
     });
 
     if (!response.ok) {
@@ -42,7 +37,6 @@ export const detectText = async (image) => {
     }
 
     const text = await response.text();
-    console.log("text is ", text)
     return text; // Return the detected text
   } catch (error) {
     console.error(error);
